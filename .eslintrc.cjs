3459915c26db408809ff0c707992e5b6e30e6ad7
Mateusz Owczarek <mateuszowczarek@onet.eu>
2024-03-02 06:38:32 +0100

    :art: implemented eslint with prettier, typescript and react plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,30 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  env: {
+    browser: true,
+    es2021: true,
   },
-}
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-no-bind": 0,
+    "@typescript-eslint/no-unused-vars": 0,
+    "react/no-unescaped-entities": 0,
+    "import/no-extraneous-dependencies": 0,
+    "react-hooks/exhaustive-deps": 0,
+  },
+};
--- src/App.tsx
+++ src/App.tsx
@@ -2,58 +2,57 @@ import axios, { AxiosError, AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 
 interface CurrencyInfo {
-  rank: number,
-  id: string,
-  supply: number,
-  maxSupply: number,
-  marketCapUsd: number,
-  volumeUsd24H: number,
-  priceUsd: number,
-  changePercent24H: number,
-  vwap24hr: number
+  rank: number;
+  id: string;
+  supply: number;
+  maxSupply: number;
+  marketCapUsd: number;
+  volumeUsd24H: number;
+  priceUsd: number;
+  changePercent24H: number;
+  vwap24hr: number;
 }
 
 type CryptoResponse = {
-  data: CurrencyInfo[],
-  timestamp: Date
-}
+  data: CurrencyInfo[];
+  timestamp: Date;
+};
 
 function App() {
-const [crypto, setCrypto] = useState<CryptoResponse>();
+  const [crypto, setCrypto] = useState<CryptoResponse>();
 
-useEffect(() => {
-    axios.get('https://api.coincap.io/v2/assets')
-    .then(function (response: AxiosResponse<CryptoResponse, AxiosError>) {
-      setCrypto(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {
-      console.info('Done');
-    });
-
-}, [])
+  useEffect(() => {
+    axios
+      .get("https://api.coincap.io/v2/assets")
+      .then(function (response: AxiosResponse<CryptoResponse, AxiosError>) {
+        setCrypto(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        console.info("Done");
+      });
+  }, []);
 
   return (
-    <>
     <h1>
-      {crypto && crypto.data.map((e) => {
-        return <ul>
-          <li key={e.rank}>{e.id}: {e.priceUsd.toString().slice()}</li>
-        </ul>
-      })  
-      }
-      
+      {crypto &&
+        crypto.data.map((e) => {
+          return (
+            <ul>
+              <li key={e.rank}>
+                {e.id}: {e.priceUsd.toString().slice()}
+              </li>
+            </ul>
+          );
+        })}
     </h1>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
 
-
-// 
-
+//
 
 // https://api.coincap.io/v2/assets
